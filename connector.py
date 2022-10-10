379395bc26b0e506f8e0d1918f8923beca662046
Makario <mararkarp@gmail.com>
2022-10-10 23:56:36 +0200

    reproduced logic from the tutorial, seems to be working so far

--- connector.py
+++ connector.py
@@ -5,7 +5,7 @@ class Connector:
     """
     Object aimed at facilitating the interaction with companies house API
     """
-    def __init__(self, rate: int, limit: asyncio.Semaphore):
+    def __init__(self, rate: int, limit: int):
         self.rate = rate,
         self.limit = limit,
         #Keep lists for now, iterate later
@@ -34,5 +34,42 @@ class Connector:
     async def requestCompanyOfficers():
         return
 
-    async def createRequestQueue(self, requestList):
-        await asyncio.gather(*requestList)
+async def sendRequests(urlList, headerList, auth, rate, limit):
+    limit = asyncio.Semaphore(limit)
+    conn = Connector(rate = rate, limit = limit)
+    tasks = []
+    for url, headers in zip(urlList, headerList):
+        tasks.append(conn.requestCompaniesSearch(url = url, headers = headers, auth = auth))
+    await asyncio.gather(*tasks)
+    return conn
+
+##########Rewriting a new object for the sake of test: it works
+class Fetch:
+    def __init__(self, limit, rate):
+        self.limit = limit
+        self.rate = rate
+        self.searchStorage = []
+
+    async def make_request(self, url, auth, params):
+        async with self.limit:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url = url, auth = auth, params = params) as resp:
+                    data_js = await resp.json(content_type = None) # this works
+                    status = resp.status
+                    rUrl = resp.url
+                    print(f"Status: {status}, URL: {rUrl}")
+                    await asyncio.sleep(self.rate)
+                    self.searchStorage.append(data_js)
+                    #We can do a return here?
+async def mainFetch(urlList, paramList, rate, limit, auth):
+    limit = asyncio.Semaphore(limit)
+    fetcher = Fetch(
+        rate = rate,
+        limit = limit
+    )
+    tasks = []
+
+    for url, params in zip(urlList, paramList):
+        tasks.append(fetcher.make_request(url = url, auth = auth, params = params))
+    results = await asyncio.gather(*tasks)
+    return results
