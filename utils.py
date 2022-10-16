f50884de18edd91066a1cef5745fae2a80765d5f
Makario <mararkarp@gmail.com>
2022-10-16 15:44:13 +0200

    Added functions to adjust days_back param

--- utils.py
+++ utils.py
@@ -1,4 +1,14 @@
 from datetime import timedelta, date
 
+def getDaysDelta(lastLeadDate: date):
+        delta = date.today() - lastLeadDate
+        return delta.days + 1 #adding 1 to cover overlapping day as well!
+
 def createSearchDates(daysBack):
         return [date.today() - timedelta(days = x) for x in range(daysBack)]
+
+def createParams(headerBase: dict, day: date):
+        output = headerBase
+        output["incorporated_from"] = str(day)
+        output["incorporated_to"] = str(day)
+        return output
