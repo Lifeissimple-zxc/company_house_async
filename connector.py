0d41368a4af762e133490527d27c080773b82ebd
Makario <mararkarp@gmail.com>
2022-10-16 15:43:48 +0200

    Updated connector object to work async

--- connector.py
+++ connector.py
@@ -1,75 +1,43 @@
 import aiohttp
 import asyncio
+from attr import define
 
+@define
 class Connector:
-    """
-    Object aimed at facilitating the interaction with companies house API
-    """
-    def __init__(self, rate: int, limit: int):
-        self.rate = rate,
-        self.limit = limit,
-        #Keep lists for now, iterate later
-        self.searchStorage = []
-        self.companyStorage = []
-        self.officerStorage = []
+    rate: int
+    limit: asyncio.Semaphore
 
-    async def requestCompaniesSearch(self, url, headers, auth):
-        """
-        Function for performing advances company search.
-        Params (headers) are expected to come from OPS Gsheet.
-        """
-        async with self.limit:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url = url, auth = auth, headers = headers) as resp:
-                    json = await resp.json()
-                    status = resp.status
-                    await asyncio.sleep(self.rate)
-                    self.searchStorage.append(json)
-    
-    
-    async def requestCompany():
-        return
-    
-    
-    async def requestCompanyOfficers():
-        return
-
-async def sendRequests(urlList, headerList, auth, rate, limit):
-    limit = asyncio.Semaphore(limit)
-    conn = Connector(rate = rate, limit = limit)
-    tasks = []
-    for url, headers in zip(urlList, headerList):
-        tasks.append(conn.requestCompaniesSearch(url = url, headers = headers, auth = auth))
-    await asyncio.gather(*tasks)
-    return conn
-
-##########Rewriting a new object for the sake of test: it works
-class Fetch:
-    def __init__(self, limit, rate):
-        self.limit = limit
-        self.rate = rate
-        self.searchStorage = []
-
-    async def make_request(self, url, auth, params):
+    async def makeRequest(self, url, auth, params, storage: list):
         async with self.limit:
             async with aiohttp.ClientSession() as session:
                 async with session.get(url = url, auth = auth, params = params) as resp:
-                    data_js = await resp.json(content_type = None) # this works
+                    await asyncio.sleep(self.rate)
+                    dataJson  = await resp.json(content_type = None)
                     status = resp.status
                     rUrl = resp.url
+                    #Check if data JSON is none - log this
+                    if dataJson is not None:
+                        storage += dataJson["items"]
+                    #Log request result
                     print(f"Status: {status}, URL: {rUrl}")
-                    await asyncio.sleep(self.rate)
-                    self.searchStorage.append(data_js)
-                    #We can do a return here?
-async def mainFetch(urlList, paramList, rate, limit, auth):
+                    #TO-DO: retries if we hit 429? Maybe store to cache and use a separate function to query those?
+
+async def makeRequests(urlList, rate, limit, auth, storage, paramList = None):
     limit = asyncio.Semaphore(limit)
-    fetcher = Fetch(
+    fetcher = Connector(
         rate = rate,
         limit = limit
     )
+
     tasks = []
 
-    for url, params in zip(urlList, paramList):
-        tasks.append(fetcher.make_request(url = url, auth = auth, params = params))
-    results = await asyncio.gather(*tasks)
-    return results
+    if paramList is not None:
+        for url, params in zip(urlList, paramList):
+            tasks.append(fetcher.makeRequest(url = url, auth = auth, params = params, storage = storage))
+    else:
+        for url in urlList:
+            tasks.append(fetcher.makeRequest(url = url, auth = auth, params = None, storage = storage))
+    
+    await asyncio.gather(*tasks)
+
+    return True
