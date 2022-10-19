3abe3dea9f3b2966d55df5b03082077459d9a1e8
Makario <mararkarp@gmail.com>
2022-10-19 23:45:21 +0200

    Reverted back to classic syntax because it enables IDE to give hits for class inputs

--- connector.py
+++ connector.py
@@ -2,16 +2,15 @@ import aiohttp
 import asyncio
 from attr import define
 
-@define
 class Connector:
-    rate: int
-    limit: asyncio.Semaphore
+    def __init__(self, rate: int, limit: asyncio.Semaphore):
+        self.rate = rate
+        self.limit = limit
 
     async def makeRequest(self, url, auth, params, storage: list):
         async with self.limit:
             async with aiohttp.ClientSession() as session:
                 async with session.get(url = url, auth = auth, params = params) as resp:
-                    await asyncio.sleep(self.rate)
                     dataJson  = await resp.json(content_type = None)
                     status = resp.status
                     rUrl = resp.url
@@ -20,6 +19,8 @@ class Connector:
                         storage += dataJson["items"]
                     #Log request result
                     print(f"Status: {status}, URL: {rUrl}")
+                    if status == 429:
+                        await asyncio.sleep(self.rate)
                     #TO-DO: retries if we hit 429? Maybe store to cache and use a separate function to query those?
 
 async def makeRequests(urlList, rate, limit, auth, storage, paramList = None):
