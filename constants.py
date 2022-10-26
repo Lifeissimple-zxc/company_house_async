2a653440c09efbc826e7c79eb5c92c9b250374e3
Makario <mararkarp@gmail.com>
2022-10-27 00:55:35 +0200

    Added logging steps to the logic, changes happened across several files

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
 SEARCH_URL = "https://api.company-information.service.gov.uk/advanced-search/companies"
 LIMIT = 10
 RATE = 5
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+LOG_FILE_NAME = "leadTracker.log"
+LOG_FOLDER = "Logs"
+TIMEZONE = "UTC"
--- tracker.py
+++ tracker.py
@@ -1,51 +1,86 @@
 import asyncio
 import os
+import re
 import pandas as pd
 import json
+import logging
+import sys
+from pytz import timezone
 from leadManager import LeadManager
-from utils import createSearchDates, createParams, getDaysDelta, performTasks
+from utils import createSearchDates, createParams, getDaysDelta, softDirCreate
+from asyncUtils import performTasks
 from connector import Connector
 from dotenv import load_dotenv
 from aiohttp import BasicAuth
-from sys import platform
 from sheetManager import sheetManager
 from datetime import datetime as dt
-from constants import SEARCH_URL, LIMIT, RATE
-#Read ENV variables
+from constants import (
+    SEARCH_URL,
+    LIMIT,
+    RATE,
+    LOG_FORMAT,
+    LOG_FILE_NAME,
+    LOG_FOLDER,
+    TIMEZONE
+)
+from logging import handlers
+from customLogger import customLogger
+
+#Read ENV variables - pass to a separrate function or file?
 load_dotenv()
 REST_URL = os.getenv("REST_BASE_URL")
 REST_KEY = os.getenv("REST_KEY")
-GSHEET_SECRET = os.getenv("GSHEET_SECRET")
+GSHEET_SECRET = json.loads(os.getenv("GSHEET_SECRET"))
 GSHEET_ID = os.environ.get("GSHEET_ID")
-BENCHMARK_SHEETNAMES = os.getenv("BENCHMARK_SHEETNAMES")
+BENCHMARK_SHEETNAMES = os.getenv("BENCHMARK_SHEETNAMES").split(",")
 GSHEET_CONTROL_PANEL_NAME = os.getenv("GSHEET_CONTROL_PANEL_NAME")
 GSHEET_LEAD_TABLE_NAME = os.getenv("GSHEET_LEAD_TABLE_NAME")
-
-#Read secrets from .env strings
-sheetSecrets = json.loads(GSHEET_SECRET)
-benchmarkSheets = BENCHMARK_SHEETNAMES.split(",")
-#Test params
-TEST_PARAMS = {'headers': {'company_status': 'active,open', 'sic_codes': '56101,56102,56103'}, 'days_back': 30}
+#Create logger dir, TO-DO: check if error can be handled here
+softDirCreate(LOG_FOLDER)
+#Configure logger
+logging.basicConfig(
+    level = logging.INFO,
+    format = LOG_FORMAT,
+    handlers = [
+        handlers.TimedRotatingFileHandler(
+            when = "midnight",
+            utc = True,
+            filename = os.path.join(LOG_FOLDER, LOG_FILE_NAME)
+        ),
+        logging.StreamHandler() #this should write to console?
+    ]
+)
+#Set logger timezone
+logging.Formatter.converter = lambda *args: dt.now(tz=timezone(TIMEZONE)).timetuple()
+#Perform logger assignment
+recordKeeper = customLogger(logging.getLogger())
+recordKeeper.logger.info("Logger instantiated")
+#Test params - remove?
+#  TEST_PARAMS = {'headers': {'company_status': 'active,open', 'sic_codes': '56101,56102,56103'}, 'days_back': 30}
 #Preapre to run async programm - move to a separate function?
-if "win" in str(platform).lower():
+if "win" in str(sys.platform).lower():
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) #windows-specific thing!
+    recordKeeper.logger.info("Event policy set, this is a windows-specific step!")
+
 #Init lead manager
 manager = LeadManager()
-
+recordKeeper.logger.info("Sheet Manager Instantiated")
 #Read Spreadsheet Data
 sheetReader = sheetManager(
-    sheetId = GSHEET_ID,
-    sheetSecrets = sheetSecrets,
-    benchmarkSheets = benchmarkSheets,
+    logger = recordKeeper,
+    benchmarkSheets = BENCHMARK_SHEETNAMES,
     controlPanelSheetName = GSHEET_CONTROL_PANEL_NAME,
     leadsSheetName = GSHEET_LEAD_TABLE_NAME
 )
 #TO-DO: Steps here needs to be logged
-sheetReader.openSheet()
+sheetReader.connect()
+sheetReader.openSheet(GSHEET_ID)
 sheetReader.validateSheet()
 sheetReader.readControlPanel()
 countLeadsSheet = sheetReader.readLeadsTable()
 searchParams = sheetReader.parseSearchParams()
+recordKeeper.logger.info("Spreadsheet data read & parsed!")
+exit()
 #TO-DO: move this to a separate function?
 if countLeadsSheet > 0:
     maxLeadCreatedStr = str(max(pd.to_datetime(sheetReader.sheetLeadsFrame["date_of_creation"])))[:10]
