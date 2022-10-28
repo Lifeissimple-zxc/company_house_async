ec0bf54e4e538cb5311cdbd6e62113342a432e83
Makario <mararkarp@gmail.com>
2022-10-29 00:23:51 +0200

    Added path to YAML

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 SEARCH_URL = "https://api.company-information.service.gov.uk/advanced-search/companies"
-LIMIT = 10
-RATE = 5
+LIMIT = 10 #move to config?
+RATE = 5 #move to config?
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 LOG_FILE_NAME = "leadTracker.log"
 LOG_FOLDER = "Logs"
 TIMEZONE = "UTC"
+SHEET_SCHEMA_PATH = "SHEET_SCHEMA.yaml"
