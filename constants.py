c2c8e7055cc9ac513506c7e66123cab06e1e42dd
Makario <mararkarp@gmail.com>
2022-11-02 23:39:28 +0100

    minor changes, notes

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 SEARCH_URL = "https://api.company-information.service.gov.uk/advanced-search/companies"
 LIMIT = 10 #move to config?
 RATE = 5 #move to config?
-LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s" #TO-DO: show function / more context here
 LOG_FILE_NAME = "leadTracker.log"
 LOG_FOLDER = "Logs"
 TIMEZONE = "UTC"
